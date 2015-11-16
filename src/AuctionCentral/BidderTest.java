73940e8ffc35e2b8585317e91eb87881c6ef5aeb
philm9 <philm9590@hotmail.com>
2015-11-16 12:59:38 -0800

    Changed biddertest.java

--- src/AuctionCentral/BidderTest.java
+++ src/AuctionCentral/BidderTest.java
@@ -1,55 +1,47 @@
 package AuctionCentral;
-/*
- * Junit testing for bidder
- * 
- * Phillip M
- */
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class BidderTest 
 {
-	private Bidder bidderTest;
-	private Auction auctionTest;
-	private Items itemTest;
-
-	public void setUp() throws Exception
+	private Bidder bid;
+	private Calendar cal;
+	private String name;
+	
+	@Before
+	public void setUp() throws Exception 
 	{
-		bidderTest = new Bidder();
-		auctionTest = new Auction(null, null, null, null);
-		itemTest = new Items(0, null, null, 0);
+		bid = new Bidder(cal, name);
 	}
 	
-	@Test
-	public void testEnterBid() {
-		assertEquals("Test Failed", 1.00, 0.00, 2.50);
+	public void testBidder()
+	{
+		bid = new Bidder(cal, "Bob");
+		assertNotNull("Bidder creation fail, no calendar", cal);
+		assertNotNull("Bidder creation fail, no name", "Bob");
+	}
+	
+	public void testChangeBid()
+	{
+		fail("Not yet implemented");
+	}
+	
+	public void testCancelBid()
+	{
+		fail("Not yet implemented");
+	}
+	
+	public void testEnterBid()
+	{
+		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testChangeBid() {
-		assertEquals("Test Failed", 1.00, 1.50, 2.50);
-	}
-
-	@Test
-	public void testCancelBid() {
-		assertEquals("Test Failed", 0.00, 1.00, 2.50);
-	}
-
-	@Test
-	public void testViewCurrentBids() {
-		assertEquals("Test Failed", bidderTest.viewCurrentBids(), null);
-	}
-
-	@Test
-	public void testViewCurrentAuctions() {
-		assertEquals("Test Failed", bidderTest.viewCurrentAuctions(auctionTest), null);
-	}
-
-	@Test
-	public void testViewItem() {
-		assertEquals("Test Failed", bidderTest.viewItem(itemTest), null);
+	public void test() {
+		fail("Not yet implemented");
 	}
 
 }
